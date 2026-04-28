fd4514a446cda28d224cacf14fb028809f2d3c6a
aryarezalfarezi-cmyk <aryarezalfarezi@gmail.com>
2026-04-28 15:38:39 +0700

    fullkode

--- friendfunctionsatu.cpp
+++ friendfunctionsatu.cpp
@@ -1,2 +1,24 @@
 #include <iostream>
 #include <string>
+using namespace std;
+class mahasiswa
+{
+private:
+    string nama;
+
+public:
+    friend void setnama(mahasiswa &a, string);
+};
+
+void setnama(mahasiswa &a, string b)
+{
+    a.nama = b;
+    cout << a.nama;
+}
+
+int main()
+{
+    mahasiswa joko;
+    setnama(joko, "kairi kumar");
+    return 0;
+}
