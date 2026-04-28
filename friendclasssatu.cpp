a2bb87b10e2722eb3e90e1ed3a6e5f5960e75d9e
aryarezalfarezi-cmyk <aryarezalfarezi@gmail.com>
2026-04-28 15:30:35 +0700

    classorang

--- friendclasssatu.cpp
+++ friendclasssatu.cpp
@@ -1,3 +1,13 @@
 #include <iostream>
 #include <string>
 using namespace std;
+class siswa;
+class orang;
+{
+private:
+    string nama;
+
+public:
+    void setnama(string pnama);
+    friend class siswa;
+};
